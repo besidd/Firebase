551e2789e22790a99599759d256bedbe97264ee3
sidd <besidd2301@gmail.com>
2025-05-26 17:18:02 +0100

    Initial Commit

---
+++ Firebase/FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseApp.swift
+//  Firebase
+//
+//  Created by Siddharth Bhayana on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseTests/FirebaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTests.swift
+//  FirebaseTests
+//
+//  Created by Siddharth Bhayana on 26/05/25.
+//
+
+import Testing
+@testable import Firebase
+
+struct FirebaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
